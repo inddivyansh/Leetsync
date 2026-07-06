60b4e5d923c022fc94ce0798b1c9411184073144
Divyansh Nagar <143708772+inddivyansh@users.noreply.github.com>
2026-07-06 11:16:34 +0530

    Time: 4 ms (89.93%) | Memory: 11.6 MB (59.40%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+
+        int n = board.size();
+
+        vector<vector<int>> score(n, vector<int>(n, -1));
+        vector<vector<int>> ways(n, vector<int>(n, 0));
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        board[0][0] = '0';
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (board[i][j] == 'X') continue;
+                if (i == n - 1 && j == n - 1) continue;
+
+                int best = -1;
+                long long cnt = 0;
+
+                if (i + 1 < n && ways[i + 1][j]) {
+                    if (score[i + 1][j] > best) {
+                        best = score[i + 1][j];
+                        cnt = ways[i + 1][j];
+                    } else if (score[i + 1][j] == best) {
+                        cnt += ways[i + 1][j];
+                    }
+                }
+
+                if (j + 1 < n && ways[i][j + 1]) {
+                    if (score[i][j + 1] > best) {
+                        best = score[i][j + 1];
+                        cnt = ways[i][j + 1];
+                    } else if (score[i][j + 1] == best) {
+                        cnt += ways[i][j + 1];
+                    }
+                }
+
+                if (i + 1 < n && j + 1 < n && ways[i + 1][j + 1]) {
+                    if (score[i + 1][j + 1] > best) {
+                        best = score[i + 1][j + 1];
+                        cnt = ways[i + 1][j + 1];
+                    } else if (score[i + 1][j + 1] == best) {
+                        cnt += ways[i + 1][j + 1];
+                    }
+                }
+
+                if (best == -1) continue;
+
+                score[i][j] = best + (board[i][j] - '0');
+                ways[i][j] = cnt % MOD;
+            }
+        }
+
+        if (ways[0][0] == 0) return {0, 0};
+
+        return {score[0][0], ways[0][0]};
+    }
+};
