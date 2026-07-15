2136751e1211a934d4ccfbfc8df7aae82778a844
Divyansh Nagar <143708772+inddivyansh@users.noreply.github.com>
2026-07-15 17:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int gcd=0;
+        int sumo= n*n;
+        int sume= n*(n+1);
+
+        
+        return n;
+
+        
+    }
+};
