8bbb1254e7c096daf76c17e24cf51cde65052c9c
Divyansh Nagar <143708772+inddivyansh@users.noreply.github.com>
2026-08-27 17:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (70.06%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string lexGreaterPermutation(string s, string target) {
+    int n = s.size();
+    vector<int> cnt(26, 0);
+    for (char c : s) cnt[c - 'a']++;
+
+    string pref = "";
+    for (int i = 0; i < n; i++) {
+        int x = target[i] - 'a';
+        if (cnt[x] == 0) break;
+        cnt[x]--;
+        pref += target[i];
+    }
+
+    if ((int)pref.size() < n) {
+        int i = pref.size();
+        int x = target[i] - 'a';
+        for (int c = x + 1; c < 26; c++) {
+            if (cnt[c] == 0) continue;
+            string ans = pref;
+            ans += char('a' + c);
+            cnt[c]--;
+            for (int ch = 0; ch < 26; ch++) {
+                ans.append(cnt[ch], char('a' + ch));
+            }
+            return ans;
+        }
+    }
+
+    for (int i = (int)pref.size() - 1; i >= 0; i--) {
+        cnt[pref[i] - 'a']++;
+        pref.pop_back();
+        int x = target[i] - 'a';
+        for (int c = x + 1; c < 26; c++) {
+            if (cnt[c] == 0) continue;
+            string ans = pref;
+            ans += char('a' + c);
+            cnt[c]--;
+            for (int ch = 0; ch < 26; ch++) {
+                ans.append(cnt[ch], char('a' + ch));
+            }
+            return ans;
+        }
+    }
+
+    return "";
+}
+};
